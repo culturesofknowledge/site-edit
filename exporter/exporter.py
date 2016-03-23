866a185a23ec88a37a57e48ac2021f6b1d72bf99
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-03-23 14:22:30 +0000

    Add work, people, locations and comments etctractors.

--- exporter/exporter.py
+++ exporter/exporter.py
@@ -2,31 +2,41 @@
 from __future__ import print_function
 
 __author__ = 'matthew'
-
 import os.path
+import csv
 
 import psycopg2
 import psycopg2.extras
 
-import csv_unicode
-import objects
-
+import exporter.objects
 
 class Exporter:
 
 	def __init__(self, postgres_connection, output=False ):
 
-		self.connection = psycopg2.connect( postgres_connection )  # e.g. "dbname='ouls' user='postgres' host='localhost' password=''"
+		self.connection = psycopg2.connect( postgres_connection )  # e.g. "dbname='?' user='?' host='?' password='?'"
 		self.cursor = self.connection.cursor( cursor_factory=psycopg2.extras.DictCursor )
 
 		self.output_commands = output
 
 		self.relationship_ids = None
 
+		self.names = {
+			"work" : "work",
+			"location" : "location",
+			"person" : "person",
+			"manifestation" : "manifestation",
+			"institution" : "institution",
+			"image" : "image",
+			"resource" : "resource",
+			"comment" : "comment",
+			"relationship" : "relationship"
+		}
+
 
 	def export(self, work_ids, output_folder ):
 		"""
-			Create csv files associated with the workids
+			Create csv files associated with the workids (This can use a lot of memory... given a lot of works)
 
 			:param work_ids: Work ids to collect information on and export
 			:param output_folder: The place to export the files too.
@@ -35,29 +45,42 @@ class Exporter:
 
 		if not self._empty( work_ids ) :
 
-			work = "work"
-			location = "location"
-			person = "person"
+			#work = "work"
+			#location = "location"
+			#person = "person"
+			#comment = "comment"
 
 			self.relationship_ids = []
 
-			work_ids = set(work_ids)  # ensure unique
 
 			if not os.path.exists( output_folder ):
 				os.makedirs( output_folder )
 
+			work_ids = set(work_ids)  # ensure unique
+
 			# Get Works (sub works of works!)
 			# Disabling this, it
-			# work_ids = work_ids.union( self._get_relationships( work, work_ids, work ) )
+			# work_ids = work_ids.union( self._get_relationships( self.names['work'], work_ids, self.names['work'] ) )
 
-
-			# Get Locations from works
-			#location_ids = self._get_relationships( work, work_ids, location )
+			works = self._get_works( list(work_ids) )
 
 			# Get People from works
-			person_ids = self._get_relationships( work, work_ids, person )
+			person_ids = self._get_relationships( self.names['work'], work_ids, self.names['person'] )
+			people = self._get_people( person_ids )
 
+			print( self._get_person_field( people[0], "foaf_name" ) )
 
+			# Get Locations from works
+			#location_ids = self._get_relationships( self.names['work'], work_ids, self.names['location'] )
+			#locations = self._get_locations( location_ids )
+
+			# Get comments associated with works
+			#comment_ids = self._get_relationships( self.names['work'], work_ids, self.names['comment'] )
+			#work_comments = self._get_comments( comment_ids )
+
+	def _create_work_csv(self, people, locations, comments):
+		#get_work_csv_converter
+		pass
 
 	def _get_relationships(self, object_name, object_ids, wanted_name ):
 		"""
@@ -72,10 +95,10 @@ class Exporter:
 		wanted_ids = set()
 
 		object_table = "cofk_union_" + object_name
-		object_ids_in_query = self._in(object_ids)
+		object_ids_in_query = self._create_in(object_ids)
 		wanted_table = "cofk_union_" + wanted_name
 
-		raw_command = "SELECT relationship_id,left_table_name,right_table_name,left_id_value,right_id_value FROM cofk_union_relationship"
+		raw_command = "SELECT relationship_id,left_table_name,right_table_name,left_id_value,right_id_value,relationship_type FROM cofk_union_relationship"
 		raw_command += " WHERE (left_table_name=%s AND right_table_name=%s AND left_id_value IN " + object_ids_in_query + ")"
 		raw_command += " OR (left_table_name=%s AND right_table_name=%s AND right_id_value IN " + object_ids_in_query + ")"
 
@@ -84,11 +107,13 @@ class Exporter:
 		if len(relations) > 0 :
 
 			for relation in relations :
-				if relation['left_table_name'] == wanted_table:
-					wanted_ids.add( relation['left_id_value'] )
-				else:
-					wanted_ids.add( relation['right_id_value'] )
 
+				if relation['left_table_name'] == wanted_table:
+					want_id = relation['left_id_value']
+				else:
+					want_id = relation['right_id_value']
+
+				wanted_ids.add( want_id )
 				self.relationship_ids.append( str(relation['relationship_id']) )
 
 			#print( relations )
@@ -97,12 +122,56 @@ class Exporter:
 
 		return wanted_ids
 
-	def get_people(self, ids ):
-		fields = objects.get_people_fields()
+	def _get_works(self, ids ):
+		fields = exporter.objects.get_work_fields()
 
-		command = "SELECT " + ",".join(fields) + " FROM cofk_union_" + object_type + " WHERE " + object_type + "_id in " + self._in( object_ids )
+		works = self._get_objects( self.names['work'], ids, fields )
+
+		print( "works: ", works )
+
+		return works
+
+	def _get_people(self, ids ):
+		fields = exporter.objects.get_person_fields()
+
+		people = self._get_objects( self.names['person'], ids, fields )
+
+		print( "people: ", people )
+
+		return people
+
+	def _get_person_field(self, person, field ):
+		return self._get_object_field( person, field, exporter.objects.get_person_fields() )
+
+	def _get_locations(self, ids ):
+		fields = exporter.objects.get_location_fields()
+
+		locations = self._get_objects( self.names['location'], ids, fields )
+
+		print( "locations: ", locations )
+
+		return locations
+
+	def _get_comments(self, ids ):
+
+		fields = exporter.objects.get_comment_fields()
+
+		comment = self._get_objects( self.names['comment'], ids, fields )
+
+		print( "comments: ", comment )
+
+		return comment
+
+	def _get_object_field(self,obj,field,all_fields):
+		return obj[all_fields.index(field)]
+
+	def _get_objects(self, object_type, object_ids, object_fields ):
+
+		command = "SELECT " + ",".join(object_fields) + " FROM cofk_union_" + object_type + " WHERE " + object_type + "_id in " + self._create_in( object_ids )
 
 		data = self.select_all( command )
+
+		return data
 
 
 	def select_one(self, raw_command, *args ):
@@ -131,8 +200,14 @@ class Exporter:
 		else:
 			return self.cursor.fetchall()
 
+	@staticmethod
+	def _empty(lst ):
+		return lst is None or len(lst) == 0
 
 	@staticmethod
 	def _output( *args ):
 		print( " ".join( [ str(item) for item in args]) )
 
+	@staticmethod
+	def _create_in(group):
+		return "('" + "','".join(group) + "')"
--- exporter/objects.py
+++ exporter/objects.py
@@ -1,12 +1,128 @@
 # -*- coding: utf-8 -*-
 
+def get_work_fields() :
+	return [
+		"work_id",
+		"description",
+		"date_of_work_as_marked",
+		"original_calendar",
+		"date_of_work_std",
+		"date_of_work_std",
+		"date_of_work_std_year",
+		"date_of_work_std_month",
+		"date_of_work_std_day",
+		"date_of_work2_std_year",
+		"date_of_work2_std_month",
+		"date_of_work2_std_day",
+		"date_of_work_std_is_range",
+		"date_of_work_inferred",
+		"date_of_work_uncertain",
+		"date_of_work_approx",
+		"authors_as_marked",
+		"addressees_as_marked",
+		"authors_inferred",
+		"authors_uncertain",
+		"addressees_inferred",
+		"addressees_uncertain",
+		"destination_as_marked",
+		"origin_as_marked",
+		"destination_inferred",
+		"destination_uncertain",
+		"origin_inferred",
+		"origin_uncertain",
+		"abstract",
+		"keywords",
+		"language_of_work",
+		"work_is_translation",
+		"incipit",
+		"explicit",
+		"ps",
+		"original_catalogue",
+		"accession_code",
+		"work_to_be_deleted",
+		"iwork_id",
+		"editors_notes",
+		#"edit_status",
+		#"relevant_to_cofk",
+		#"creation_timestamp",
+		#"creation_user",
+		#"change_timestamp",
+		#"change_user"
+	]
 
+# o=object, f=field, r=relationship
+def get_work_csv_converter() :
 
-def get_people_fields() :
+	translation = {
+		"EMLO ID Number" : { "o" : "work", "f" : "iwork_id" },
+		"Year date" : { "o" : "work", "f" : "date_of_work_std_year"},
+		"Month date" : { "o" : "work", "f" : "date_of_work_std_month"},
+		"Day date" : { "o" : "work", "f" : "date_of_work_std_day"},
+		"Date is range" : { "o" : "work", "f" : "date_of_work_std_is_range"},
+		"Year 2nd date (range)" : { "o" : "work", "f" : "date_of_work2_std_year"},
+		"Month 2nd date (range)" : { "o" : "work", "f" : "date_of_work2_std_month"},
+		"Day 2nd date (range)" : { "o" : "work", "f" : "date_of_work2_std_day"},
+		"Calendar of date provided to EMLO" : { "o" : "work", "f" : "original_calendar"},
+		"Date as marked on letter" : { "o" : "work", "f" : "date_of_work_as_marked"},
+		"Date uncertain" : { "o" : "work", "f" : "date_of_work_uncertain"},
+		"Date approximate" : { "o" : "work", "f" : "date_of_work_approx"},
+		"Date inferred" : { "o" : "work", "f" : "date_of_work_inferred"},
+		"Notes on date" : { "o" : "comment", "f" : "comment", "r" : "refers_to_date"},
+
+		"Author" :  { "o" : "person", "f" : "foaf_name", "r" : "created" },
+		"Author ID" : { "o" : "person", "f" : "iperson_id", "r" : "created" },
+		"Author as marked" : { "o" : "work", "f" : "authors_as_marked"},
+		"Author inferred" : { "o" : "work", "f" : "authors_inferred"},
+		"Author uncertain" : { "o" : "work", "f" : "authors_uncertain"},
+		"Notes on Author re. Letter" : { "o" : "comment", "f" : "comment", "r" : "refers_to_author"},
+		"Recipient" : { "o" : "person", "f" : "foaf_name", "r" : "was_addressed_to" },
+		"Recipient ID" : { "o" : "person", "f" : "iperson_id", "r" : "was_addressed_to" },
+		"Recipient as marked" : { "o" : "work", "f" : "addressees_as_marked"},
+		"Recipient inferred" : { "o" : "work", "f" : "addressees_inferred"},
+		"Recipient uncertain" : { "o" : "work", "f" : "addressees_uncertain"},
+		"Notes on Recipient re. Letter" : { "o" : "comment", "f" : "comment", "r" : "refers_to_addressee"},
+
+		"Origin name" : { "o" : "location", "f" : "location_name","r" : "was_sent_from" },
+		"Origin ID" : { "o" : "location", "f" : "location_id","r" : "was_sent_from" },
+		"Origin as marked" : { "o" : "work", "f" : "origin_as_marked"},
+		"Origin inferred" : { "o" : "work", "f" : "origin_inferred"},
+		"Origin uncertain" : { "o" : "work", "f" : "origin_uncertain"},
+		"Destination name" : { "o" : "location", "f" : "location_name","r" : "was_sent_to" },
+		"Destination ID" : { "o" : "location", "f" : "location_id","r" : "was_sent_to" },
+		"Destination as marked" : { "o" : "work", "f" : "destination_as_marked"},
+		"Destination inferred" : { "o" : "work", "f" : "destination_inferred"},
+		"Destination uncertain" : { "o" : "work", "f" : "destination_uncertain"},
+
+		"Abstract" : { "o" : "work", "f" : "abstract"},
+		"Keywords" : { "o" : "work", "f" : "keywords"},
+		"Language(s)" : { "o" : "work", "f" : "language_of_work"},
+		"Incipit" : { "o" : "work", "f" : "incipit"},
+		"Explicit" : { "o" : "work", "f" : "explicit"},
+
+		# Complicated...
+		#"People mentioned" : { "o" : "work", "f" : ""},
+		#"EMLO ID people mentioned" : { "o" : "work", "f" : ""},
+		#"Notes on people mentioned" : { "o" : "work", "f" : ""},
+
+		# Ignoring as would include other works (complicated work connections...)
+		#"Letter in reply to" : { "o" : "work", "f" : ""},
+		#"Letter answered by" : { "o" : "work", "f" : ""},
+		#"Matching letter(s) in alternative EMLO catalogue(s)" : { "o" : "work", "f" : ""},
+
+		# This will be a separate table... some how...
+		#"Related Resource descriptor" : { "o" : "work", "f" : ""},
+		#"Related Resource URL" : { "o" : "work", "f" : ""},
+		"General notes for public display" : { "o" : "work", "f" : ""},
+		"Editors’ notes" : { "o" : "work", "f" : "editors_notes"},
+
+	}
+
+	return translation
+
+def get_person_fields() :
 
 	fields = [
-		"iperson_id",
-		#"person_id",
+		"person_id",
 		"foaf_name",
 		"skos_altlabel",
 		"skos_hiddenlabel",
@@ -27,6 +143,7 @@ def get_people_fields() :
 		"date_of_death_approx",
 		"gender",
 		"is_organisation",
+		"iperson_id",
 		#"creation_timestamp",
 		#"creation_user",
 		#"change_timestamp",
@@ -57,3 +174,38 @@ def get_people_fields() :
 
 	return fields
 
+
+def get_location_fields() :
+	fields = [
+		"location_id",
+		"location_name",
+		"latitude",
+		"longitude",
+		#"creation_timestamp",
+		#"creation_user",
+		#"change_timestamp",
+		#"change_user",
+		"location_synonyms",
+		"editors_notes",
+		"element_1_eg_room",
+		"element_2_eg_building",
+		"element_3_eg_parish",
+		"element_4_eg_city",
+		"element_5_eg_county",
+		"element_6_eg_country",
+		"element_7_eg_empire"
+	]
+
+	return fields
+
+def get_comment_fields() :
+	fields = [
+		"comment_id",
+		"comment",
+		#"creation_timestamp",
+		#"creation_user",
+		#"change_timestamp",
+		#"change_user"
+	]
+
+	return fields
