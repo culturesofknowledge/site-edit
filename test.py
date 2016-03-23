69d057840a0c59445af1e4dc3f6b1748c8ca60a0
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-03-23 17:05:10 +0000

    Get work test

--- test.py
+++ test.py
@@ -13,19 +13,26 @@ class TestExport(unittest.TestCase) :
 														+ " host='" + config["host"] + "' port='" + config["port"] + "'" \
 														+ " user='" + config["user"] + "' password='" + config["password"] + "'"
 
+		self.output_commands = False
+		self.output_results = True
+
 	def tearDown(self):
 		pass
 
-	def test_database_connection(self):
+	def test_010_database_connection(self):
+
+		print( "*** database connection test ***" )
 
 		self.assertRaises( psycopg2.OperationalError, Exporter, "dbname='' user='' host='' password=''" )
 
 		#e = exporter.Exporter( self.postgres_connection )
 
 
-	def test_select_single(self):
+	def test_020_select_single(self):
 
-		e = Exporter( self.postgres_connection )
+		print( "*** select single test ***" )
+
+		e = Exporter( self.postgres_connection, self.output_commands, self.output_results )
 
 		raw_command = "select iwork_id from cofk_union_work where work_id='cofk_import_ead-ead_c01_id:000013683'"
 		value = e.select_one( raw_command )
@@ -38,9 +45,11 @@ class TestExport(unittest.TestCase) :
 		self.assertEqual( value[0], 13683 )
 
 
-	def test_select_all(self):
+	def test_030_select_all(self):
 
-		e = Exporter( self.postgres_connection )
+		print("*** select all test ***")
+
+		e = Exporter( self.postgres_connection, self.output_commands, self.output_results )
 
 		raw_command = "select iwork_id from cofk_union_work where work_id='cofk_import_ead-ead_c01_id:000013683' or work_id='cofk_import_ead-ead_c01_id:000013690'"
 		value = e.select_all( raw_command )
@@ -57,12 +66,32 @@ class TestExport(unittest.TestCase) :
 		self.assertEqual( value[1]['iwork_id'], 13690 )
 
 
-	def test_create_csvs(self):
+	def test_040_get_work(self):
 
-		e = Exporter( self.postgres_connection )
+		print( "*** get work test ***")
+
+		e = Exporter( self.postgres_connection, self.output_commands, self.output_results )
+		work_id = 'cofk_import_hartlib-row_id:000003994'  # just some random work I chose.
+
+		works = e._get_works( [work_id] )
+
+		self.assertEqual( len(works), 1 )
+		work = works[0]
+
+		self.assertEqual( work[0], work_id )
+		self.assertEqual( e._get_work_field(work, "work_id"), work_id )
+
+		self.assertEqual( e._get_work_field(work, "original_catalogue"), "HARTLIB" )
+
+
+	def test_050_create_csvs(self):
+
+		print("*** create csvs test ***")
+
+		e = Exporter( self.postgres_connection, self.output_commands, self.output_results )
 
 		folder = "Single Export"
-		e.export( ['cofk_import_hartlib-row_id:000003994'], folder )
+		e.export( ['cofk_import_ead-ead_c01_id:000033695'], folder )
 
 
 if __name__ == "__main__":
