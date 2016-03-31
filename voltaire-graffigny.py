268931fa990d14188f583e8790c74f4c0262daf1
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-03-31 15:14:45 +0100

    voltaire graffigny export

---
+++ voltaire-graffigny.py
@@ -0,0 +1,18 @@
+__author__ = 'matthew'
+
+from exporter.exporter import Exporter
+from config import config
+
+debug_on = True
+postgres_connection = "dbname='" + config["dbname"] + "'" \
+                      + " host='" + config["host"] + "' port='" + config["port"] + "'" \
+                      + " user='" + config["user"] + "' password='" + config["password"] + "'"
+
+e = Exporter( postgres_connection, False, debug_on )
+
+command = "select work_id from cofk_union_work where original_catalogue='GRAFFIGNY'"
+
+work_ids = e.select_all( command )
+work_ids = [id['work_id'] for id in work_ids]
+
+e.export( work_ids, "voltaire-grif-test" )
