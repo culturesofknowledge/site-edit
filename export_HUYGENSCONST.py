53648d74fd882a04c94370da25b82de405dda665
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2016-08-04 16:57:38 +0100

    New exports

---
+++ export_HUYGENSCONST.py
@@ -0,0 +1,22 @@
+__author__ = 'matthew'
+
+from exporter.exporter import Exporter
+from config import config
+
+debug_on = True
+catalogue = 'HUYGENSCONST'
+
+
+
+postgres_connection = "dbname='" + config["dbname"] + "'" \
+                      + " host='" + config["host"] + "' port='" + config["port"] + "'" \
+                      + " user='" + config["user"] + "' password='" + config["password"] + "'"
+
+e = Exporter( postgres_connection, False, debug_on )
+
+command = "select work_id from cofk_union_work where original_catalogue='" + catalogue + "'"
+
+work_ids = e.select_all( command )
+work_ids = [id['work_id'] for id in work_ids]
+
+e.export( work_ids, catalogue )
