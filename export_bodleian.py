717db03896a762599c333a305ccfcb7591bffbaa
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2017-01-17 14:20:58 +0000

    Fix debug variable

--- export_bodleian.py
+++ export_bodleian.py
@@ -3,12 +3,12 @@ __author__ = 'matthew'
 from exporter.exporter import Exporter
 from config import config
 
-debug_on = True
+debug_on = False
 postgres_connection = "dbname='" + config["dbname"] + "'" \
                       + " host='" + config["host"] + "' port='" + config["port"] + "'" \
                       + " user='" + config["user"] + "' password='" + config["password"] + "'"
 
-e = Exporter( postgres_connection, False, True )
+e = Exporter( postgres_connection, debug_on, True )
 
 command = "select work_id from cofk_union_work where original_catalogue='cardindex'"
 
