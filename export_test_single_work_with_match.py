15f536cebfa296111b78683927177d834d5d83ab
Matthew Wilcoxson <git@akademy.co.uk>
2017-07-03 17:18:39 +0100

    Test match

---
+++ export_test_single_work_with_match.py
@@ -0,0 +1,19 @@
+
+from exporter.exporter import Exporter
+from config import config
+
+debug_on = True
+postgres_connection = "dbname='" + config["dbname"] + "'" \
+						+ " host='" + config["host"] + "' port='" + config["port"] + "'" \
+						+ " user='" + config["user"] + "' password='" + config["password"] + "'"
+
+e = Exporter( postgres_connection, False, debug_on )
+
+folder = "Single Export"
+
+iworkid = '8540'
+command = "select work_id from cofk_union_work where iwork_id=" + iworkid
+work_ids = e.select_all( command )
+work_id = work_ids[0]['work_id']
+
+e.export( [work_id], folder )
