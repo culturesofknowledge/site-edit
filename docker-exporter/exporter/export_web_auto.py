64bf36d90ae04b740aa9a460c265f002f6a55df3
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-02-01 18:13:48 +0000

    Folder watch for processing of exports

--- docker-exporter/exporter/export_web_auto.py
+++ docker-exporter/exporter/export_web_auto.py
@@ -4,6 +4,7 @@ import json
 import sys
 from shutil import copyfile
 import os.path
+import time
 
 import smtplib
 from email.mime.text import MIMEText
@@ -12,41 +13,84 @@ from exporter.exporter import Exporter
 from config import config
 
 debug_on = False
-postgres_connection = "dbname='" + config["dbname"] + "'" \
-                      + " host='" + config["host"] + "' port='" + config["port"] + "'" \
-                      + " user='" + config["user"] + "' password='" + config["password"] + "'"
+postgres_connection = \
+	"dbname='" + config["dbname"] + "'" + \
+	" host='" + config["host"] + "' port='" + config["port"] + "'" + \
+	" user='" + config["user"] + "' password='" + config["password"] + "'"
 
-e = Exporter( postgres_connection, False, debug_on )
+path_to_watch = "/usr/src/app/exporter_data"
+output_folder = "/usr/src/app/exports/"  # end with '/'
 
-file_pos = sys.argv[1]
 
-file_folder, file_name = os.path.split( file_pos )
-names = file_name.split(".")
+def process_file( file_pos ):  # file_pos e.g.  "/some/where/23443554.3345.json"
 
-output_name = names[0] + "." + names[1]
-output_filename =  output_name + ".xlsx";
-output_pos = file_folder + "/" + output_filename
+	e = Exporter( postgres_connection, False, debug_on )
 
-j = json.load( open( file_pos, "r" ) )
+	process_file_folder, process_file_name = os.path.split( file_pos )  # "/some/where" and "23443554.3345.json"
+	process_file_names = process_file_name.split(".")  # "23443554" "3345" "json"
 
-command = "select work_id from cofk_union_work where iwork_id in ("
-command += ",".join(j["iworkids"])
-command += ")"
+	output_name = process_file_names[0] + "." + process_file_names[1]
 
-work_ids = e.select_all( command )
-work_ids = [id['work_id'] for id in work_ids]
+	output_folder_name = "auto_" + output_name
+	output_filename = "excel_" + output_name + ".xlsx"
 
-e.export( work_ids, "auto_" + output_name, excel_output=output_pos )
+	output_pos = output_folder + "/" + output_folder_name + "/" + output_filename
 
-copyfile( output_pos, "/srv/www/exports/" + output_filename )
+	j = json.load( open( file_pos, "r" ) )
 
-msg = MIMEText("Hi, your export is now complete. You can download it here: http://emlo-edit.bodleian.ox.ac.uk/exports/" + output_filename )
-msg["Subject"] = "Your export"
-email_from = "cok_bot@emlo-edit.bodleian.ox.ac.uk"
-email_to = j['email']
-msg["From"] = email_from
-msg["To"] = email_to
+	command = "select work_id from cofk_union_work where iwork_id in ("
+	command += ",".join(j["iworkids"])
+	command += ")"
 
-s = smtplib.SMTP('localhost')
-s.sendmail( email_from, email_to, msg.as_string())
-s.quit()
+	work_ids = e.select_all( command )
+	work_ids = [id['work_id'] for id in work_ids]
+
+	e.export( work_ids, output_folder_name, excel_output=output_pos )
+
+	msg = MIMEText(
+		"Hi, your export is now complete. " +
+		"You can download it here: " +
+		"http://emlo-edit.bodleian.ox.ac.uk/exports/" +
+		output_folder_name + "/" + output_filename
+	)
+
+	msg["Subject"] = "Your export"
+	email_from = "cok_bot@emlo-edit.bodleian.ox.ac.uk"
+	email_to = j['email']
+	msg["From"] = email_from
+	msg["To"] = email_to
+
+	s = smtplib.SMTP('smtp.ox.ac.uk')
+	s.sendmail( email_from, email_to, msg.as_string())
+	s.quit()
+
+
+# file_pos = sys.argv[1]
+# process_file( file_pos )
+
+
+print "Starting export folder watch"
+
+before = dict([(f, None) for f in os.listdir (path_to_watch)])
+while 1:
+
+	# print "Checking for folder differences..."
+
+	after = dict([(f, None) for f in os.listdir( path_to_watch )])
+	added = [f for f in after if not f in before]
+	# removed = [f for f in before if not f in after]
+
+	if added:
+
+		for file_position in added :
+			if file_position.endswith( ".json") :
+				# print "processing " + file_position
+				process_file( path_to_watch + "/" + file_position )
+				time.sleep(10)
+
+	# if removed: print "Removed: ", ", ".join (removed)
+
+	before = after
+
+	time.sleep(10)
+
