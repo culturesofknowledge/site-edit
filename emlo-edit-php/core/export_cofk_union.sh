560f1f0fe884cc815cc32296f362602a4a6aa082
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-05-22 22:53:51 +0100

    Add psql for export to front end

--- emlo-edit-php/core/export_cofk_union.sh
+++ emlo-edit-php/core/export_cofk_union.sh
@@ -1,20 +1,9 @@
 #! /bin/bash
 # /home/burgess/scripts/sccs/cofk/sh/s.export_cofk_union.sh 1.6 2011/09/28 15:07:19 
 
-export COFK_CSV_DESTINATION=cofkxfer@emlo.bodleian.ox.ac.uk:/home/cofkxfer
-export COFK_CSV_MOVER=cofkadmin
+export SCRIPTDIR=/var/www/core/
+export CSVSOURCE=/csv/
 
-export SCRIPTDIR=/var/apache2/cgi-bin/aeolus/aeolus2/cofk/
-
-export CSVSOURCE_SUBDIR=csv/
-export CSVSOURCE=~/${CSVSOURCE_SUBDIR}
-
-if [ ! -d $CSVSOURCE ]
-then
-  cd
-  mkdir $CSVSOURCE_SUBDIR
-  echo "Created directory $CSVSOURCE"
-fi
 echo "Output directory will be $CSVSOURCE"
 
 #---------------------------------------------------------
@@ -66,9 +55,9 @@ function cleanup() {
     resource \
     work 
   do
-    if [ -f export_cofk_union_$tab.log ]
+    if [ -f export_cofk_union_$tabroot.log ]
     then
-      \rm export_cofk_union_$tab.log
+      \rm export_cofk_union_$tabroot.log
     fi
   done
 }
@@ -83,7 +72,7 @@ batch_size=$(( 5000 )) # evaluate as integer
 for tabroot in comment image institution location person relationship resource work
 do
   tab=cofk_union_$tabroot
-  echo "Starting $tab"ls ..
+  echo "Starting $tab"
   date
   export COFK_TABLE_TO_EXPORT=$tab
 
@@ -97,16 +86,15 @@ do
   echo "select max( $tab_id ) from $tab" > get_max_id.sql
   echo "select count( $tab_id ) from $tab" > get_rowcount.sql
 
-  psql ouls -U postgres -q  -t < get_min_id.sql > min_id.txt
-  psql ouls -U postgres -q  -t < get_max_id.sql > max_id.txt
-  psql ouls -U postgres -q  -t < get_rowcount.sql > rowcount.txt
+  psql ouls -h postgres -U postgres -q  -t < get_min_id.sql > min_id.txt
+  psql ouls -h postgres -U postgres -q  -t < get_max_id.sql > max_id.txt
+  psql ouls -h postgres -U postgres -q  -t < get_rowcount.sql > rowcount.txt
 
   first_id=$(cat min_id.txt)
-  last_id=$(cat max_id.txt)
-  rowcount=$(cat rowcount.txt)
-
   first_id=$(( $first_id )) # evaluate as integer
+  last_id=$(cat max_id.txt)
   last_id=$(( $last_id )) # evaluate as integer
+  rowcount=$(cat rowcount.txt)
 
   echo "First ID in $tab is $first_id"
   echo "Last ID in $tab is $last_id"
@@ -118,9 +106,9 @@ do
   
   while [ $COFK_LAST_ID_IN_TABLE -lt $last_id ]
   do
-    if [ $rowcount -gt $batch_size ] 
+    if [ $rowcount -gt $batch_size ]
     then
-      export COFK_LAST_ID_IN_TABLE=$(( $COFK_LAST_ID_IN_TABLE + $batch_size ))
+      export COFK_LAST_ID_IN_TABLE=$(( COFK_FIRST_ID_IN_TABLE + $batch_size ))
     else
       export COFK_LAST_ID_IN_TABLE=$last_id
     fi
@@ -137,7 +125,13 @@ do
       exit
     fi
 
-    export COFK_FIRST_ID_IN_TABLE=$(( $COFK_LAST_ID_IN_TABLE + 1 ))
+    echo "select $tab_id from $tab where $tab_id > $COFK_LAST_ID_IN_TABLE order by $tab_id limit 1" > get_nextid.sql
+    psql ouls -h postgres -U postgres -q  -t < get_nextid.sql > nextid.txt
+    next_id=$(cat nextid.txt)
+    next_id=$(( next_id )) # evaluate as integer
+
+    export COFK_FIRST_ID_IN_TABLE=$(( next_id ))
+    echo "Next id: ". ${COFK_FIRST_ID_IN_TABLE}
     export COFK_WRITE_CSV_HEADER=0
   done
 done
@@ -184,8 +178,8 @@ tab_id=relationship_code
 echo "select min( $tab_id ) from $tab" > get_min_id.sql
 echo "select max( $tab_id ) from $tab" > get_max_id.sql
 
-psql ouls -U postgres -q  -t < get_min_id.sql > min_id.txt
-psql ouls -U postgres -q  -t < get_max_id.sql > max_id.txt
+psql ouls -h postgres -U postgres -q  -t < get_min_id.sql > min_id.txt
+psql ouls -h postgres -U postgres -q  -t < get_max_id.sql > max_id.txt
 
 first_id=$(cat min_id.txt)
 last_id=$(cat max_id.txt)
@@ -224,27 +218,24 @@ ${SCRIPTDIR}reinstate_accents_selden_end.sh y
 
 #---------------------------------------------------------------------------------------
 
-psql ouls -U cofkadmin -c "\\copy pro_activity to ${CSVSOURCE}pro_activity.csv csv header"
-psql ouls -U cofkadmin -c "\\copy pro_assertion to ${CSVSOURCE}pro_assertion.csv csv header"
-psql ouls -U cofkadmin -c "\\copy pro_location to ${CSVSOURCE}pro_location.csv csv header"
-psql ouls -U cofkadmin -c "\\copy pro_primary_person to ${CSVSOURCE}pro_primary_person.csv csv header"
-psql ouls -U cofkadmin -c "\\copy pro_relationship to ${CSVSOURCE}pro_relationship.csv csv header"
-psql ouls -U cofkadmin -c "\\copy pro_role_in_activity to ${CSVSOURCE}pro_role_in_activity.csv csv header"
-psql ouls -U cofkadmin -c "\\copy pro_textual_source to ${CSVSOURCE}pro_textual_source.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_activity to ${CSVSOURCE}pro_activity.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_assertion to ${CSVSOURCE}pro_assertion.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_location to ${CSVSOURCE}pro_location.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_primary_person to ${CSVSOURCE}pro_primary_person.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_relationship to ${CSVSOURCE}pro_relationship.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_role_in_activity to ${CSVSOURCE}pro_role_in_activity.csv csv header"
+psql ouls -h postgres -U cofkadmin -c "\\copy pro_textual_source to ${CSVSOURCE}pro_textual_source.csv csv header"
 
 echo 'Export to CSV files now complete.'
 
-if [ "$LOGNAME" = "$COFK_CSV_MOVER" ]
-then
-  echo 'Copying CSV files to front end server.'
-  scp ${CSVSOURCE}*.csv $COFK_CSV_DESTINATION
+echo 'Copying CSV files to front end server.'
 
-  # New server data tranfers
-  remote_location_qa=bodl-emlo-svc@emlo-qa-site1.bodleian.ox.ac.uk:/data/emlo-docker-compose/data/
-  remote_location_prd=bodl-emlo-svc@emlo-prd-site1.bodleian.ox.ac.uk:/data/emlo-docker-compose/data/
+# New server data tranfers
+remote_location_qa=bodl-emlo-svc@emlo-qa-site1.bodleian.ox.ac.uk:/data/emlo-docker-compose/data/
+remote_location_prd=bodl-emlo-svc@emlo-prd-site1.bodleian.ox.ac.uk:/data/emlo-docker-compose/data/
 
-  for objects in comment image institution location person relationship resource work
-  do
+for objects in comment image institution location person relationship resource work
+do
     csv_local_file=${CSVSOURCE}cofk_union_${objects}.csv
 
     csv_remote_qa_file=${remote_location_qa}${objects}.csv
@@ -252,13 +243,10 @@ then
 
     csv_remote_prd_file=${remote_location_prd}${objects}.csv
     scp ${csv_local_file} ${csv_remote_prd_file}
-  done
+done
 
-  ## -- I think we'll just link to images on the back-end server instead -- ${SCRIPTDIR}transfer_uploaded_images.sh
-else
-  echo "The files in $CSVSOURCE will now need to be manually moved to the destination server."
-  ##echo 'You may also need to check for recently-uploaded images and transfer them too.'
-fi
+## -- I think we'll just link to images on the back-end server instead -- ${SCRIPTDIR}transfer_uploaded_images.sh
+
 
 cleanup  # the log files only get removed if there was no error during the process
 
