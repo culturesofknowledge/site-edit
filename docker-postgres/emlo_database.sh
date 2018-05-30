a1600f588f56cc9bf2d9a4f63afc124350d22694
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-05-30 19:05:06 +0100

    Remove obsolete file

--- docker-postgres/emlo_database.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-gunzip /tmp/pg_dumpall.out.gz
-psql -h "$POSTGRES_PORT_5432_TCP_ADDR" -p "$POSTGRES_PORT_5432_TCP_PORT" -U postgres < /tmp/pg_dumpall.out
-rm -f /tmp/pg_dumpall.out
-
