ea9a56bb58fd517bc6beb5ed932e3ca3de41d013
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-06-04 16:38:59 +0100

    Helper files for starting/updating the container group

---
+++ stop-dev.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+# Bring everything down.
+docker-compose --file docker-compose-dev.yaml down
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+echo "Nope."
+echo "If you are stopping these services you should already know what you are doing. See docker-compose."
