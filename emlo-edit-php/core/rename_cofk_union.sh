347ed6861529d16945e2afdc63e58b2f8f9a7fdc
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-06-14 13:14:37 +0100

    Add image

--- emlo-edit-php/core/rename_cofk_union.sh
+++ emlo-edit-php/core/rename_cofk_union.sh
@@ -3,6 +3,7 @@ echo 'Rename CSV files: removing the cofk_union part.'
 
 mv cofk_union_comment.csv comment.csv
 mv cofk_union_institution.csv institution.csv
+mv cofk_union_image.csv image.csv
 mv cofk_union_location.csv location.csv
 mv cofk_union_manifestation.csv manifestation.csv
 mv cofk_union_person.csv person.csv
