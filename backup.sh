6469eaaeb13cb7285b09894dde19218083e3f708
Matthew Wilcoxson <git@akademy.co.uk>
2018-08-01 17:37:56 +0100

    dump all databases.

--- backup.sh
+++ backup.sh
@@ -4,8 +4,8 @@ cd /data/emlo-editor
 . backup-helper.sh
 
 DEST=/data/backups
-FILENAME=pg-dump_ouls.gz
+FILENAME=pg-dumpall.gz
 
-docker-compose exec postgres sh -c 'pg_dump -U postgres ouls' | gzip --best > ${DEST}/${FILENAME}
+docker-compose exec postgres sh -c 'pg_dumpall -U postgres' | gzip --best > ${DEST}/${FILENAME}
 
 backup_rotate_store ${DEST} ${FILENAME}
