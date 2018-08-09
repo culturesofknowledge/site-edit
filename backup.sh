7d28a09d41f531ff98c96f4159f3ed2ebd84612c
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-08-09 17:22:09 +0100

    Fix problem with docker-compose not running under cron

--- backup.sh
+++ backup.sh
@@ -20,7 +20,7 @@ now=$(date)
 
 echo "Backing up at ${now} to ${destination}/${filename} ..."
 
-docker-compose exec postgres sh -c 'pg_dumpall -U postgres' | gzip --best > ${destination}/${filename}
+docker-compose exec -T postgres sh -c 'pg_dumpall -U postgres' | gzip --best > ${destination}/${filename}
 
 backup_rotate_store ${destination} ${filename}
 
