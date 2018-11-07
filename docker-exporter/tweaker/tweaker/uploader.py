b7729c999f08b87503fe4a1cb157a81a8b8996e5
Matthew Wilcoxson <matthew.wilcoxson@oerc.ox.ac.uk>
2018-11-07 17:05:28 +0000

    Extend and improve Uploader API.

--- docker-exporter/tweaker/tweaker/uploader.py
+++ docker-exporter/tweaker/tweaker/uploader.py
@@ -6,11 +6,20 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 	def __init__( self, connection=None, debug=False ):
 
 		DatabaseTweaker.__init__( self, connection, debug )
+		self.user = "cofkbot"
+		self.user_email = self.user + "@ai.ox.ac.uk"
+		self._id = "CokBot Uploader" #"5be3118e0160af3800a2f9b3" #
 
-		self.user_email = "cokbot@fake.ox.ac.uk"
-		self._id = "CokBot Uploader"
+		self.upload_id = None
+		self.upload_name = None
+		self.id_count_start = 2000000
+		self.id_count = self.id_count_start
 
-	def create_upload(self, name, work_count ):
+	def next_id(self):
+		self.id_count += 1
+		return self.id_count
+
+	def start_upload(self, name, work_count ):
 
 		self.check_database_connection()
 
@@ -29,7 +38,7 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 				" (_id,total_works,upload_description,upload_name,upload_status,upload_username,uploader_email,works_accepted,works_rejected)" \
 				" VALUES " \
 				" (%s,%s,%s,%s,%s,%s,%s,%s,%s)" \
-				" returning upload_id"
+				"returning upload_id"
 
 		command = self.cursor.mogrify( command, (
 			self._id,
@@ -47,15 +56,20 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 		self._audit_insert( "collect upload" )
 
 		self.cursor.execute( command )
-		return self.cursor.fetchone()[0]
+
+		self.upload_id = self.cursor.fetchone()[0]
+		self.upload_name = name
+		self.id_count = self.id_count_start
+
+		return self.upload_id
 
 
-	def create_person_existing(self, upload_id, upload_name, iperson_id ):
+	def create_person_existing(self, iperson_id ):
 
 		person = self.get_person_from_iperson_id( iperson_id )
 
 		if not person:
-			print( "Don't lie to me. They don't exist!")
+			print( "Error. Don't lie to me, that person(" + str(iperson_id) + ") does not exist!")
 
 		else :
 
@@ -66,8 +80,8 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 
 			command = self.cursor.mogrify( command, (
 				self._id,
-				upload_id,
-				upload_name,
+				self.upload_id,
+				self.upload_name,
 				iperson_id,
 				iperson_id,
 				person['person_id'],
@@ -80,7 +94,7 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 			self.cursor.execute( command )
 
 
-	def create_person_new( self, upload_id, upload_name, iperson_number, primary_name,
+	def create_person_new( self, primary_name,
 							alternative_names=None,
 
 							date_of_birth_day=None,
@@ -105,7 +119,7 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 							date_of_death2_day=None,
 							date_of_death2_month=None,
 
-							editors_notes=None,
+							editors_notes='',
 
 							flourished_day=None,
 							flourished_month=None,
@@ -117,7 +131,7 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 
 							gender='',
 							is_organisation="",
-							notes_on_person=None,
+							notes_on_person='',
 							organisation_type=None,
 							roles_or_titles=None ):
 
@@ -151,11 +165,12 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 				" VALUES " \
 				" (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
+		person_upload_id = self.next_id()
 		command = self.cursor.mogrify( command, (
 			self._id,
-			upload_id,
-			upload_name,
-			iperson_number,
+			self.upload_id,
+			self.upload_name,
+			person_upload_id,
 			primary_name,
 			alternative_names,
 
@@ -203,19 +218,22 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 
 		self.cursor.execute( command )
 
+		return person_upload_id
+
 	""" Template
-		def create_?( self, upload_id, upload_name,
-								):
+		def create_?( self, ):
 	
 			command = "INSERT INTO cofk_collect_person" \
-					  " (_id" \
-					
-					  ")" \
-					  " VALUES " \
-					  " (%s,%s)"
+				" (" \
+					"_id" \
+					",upload_id"\
+				")" \
+				" VALUES " \
+				" (%s,%s)"
 	
 			command = self.cursor.mogrify( command, (
-				_id,
+				self._id,
+				self.upload_id
 			) )
 	
 			self._print_command( "CREATE collect person", command )
@@ -223,8 +241,10 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 	
 			self.cursor.execute( command )
 	
+			
 	"""
-	def create_work( self, upload_id, upload_name, iwork_id,
+
+	def create_work( self,
 		abstract=None,
 		accession_code=None,
 		addressees_as_marked=None,
@@ -252,7 +272,6 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 		destination_inferred=0,
 		destination_uncertain=0,
 		editors_notes=None,
-		excipit=None,
 		explicit=None,
 		incipit=None,
 		keywords=None,
@@ -276,11 +295,12 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 		place_mentioned_as_marked=None,
 		place_mentioned_inferred=0,
 		place_mentioned_uncertain=0
-						   ):
+		):
 
 		union_iwork_id = None
 		work_id = None
-		upload_status=1
+		upload_status = 1
+		excipit = explicit
 
 		command = "INSERT INTO cofk_collect_work" \
 				" (" \
@@ -346,6 +366,7 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 				" VALUES " \
 				" (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
 
+		work_upload_id = self.next_id()
 		command = self.cursor.mogrify( command, (
 			self._id,
 			abstract,
@@ -378,7 +399,7 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 			excipit,
 			explicit,
 			incipit,
-			iwork_id,
+			work_upload_id,
 			keywords,
 			language_of_work,
 			mentioned_as_marked,
@@ -401,8 +422,8 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 			place_mentioned_inferred,
 			place_mentioned_uncertain,
 			union_iwork_id,
-			upload_id,
-			upload_name,
+			self.upload_id,
+			self.upload_name,
 			upload_status,
 			work_id
 		) )
@@ -412,5 +433,249 @@ class DatabaseTweakerCollect(DatabaseTweaker):
 
 		self.cursor.execute( command )
 
+		return work_upload_id
+
+	def create_location_existing( self, location_id ):
+
+		location = self.get_location_from_location_id( location_id )
+
+		if not location:
+			print( "Error. Don't lie to me, that location(" + str(location_id) + ") does not exist!")
+
+		else :
+
+			command = "INSERT INTO cofk_collect_location" \
+						" (_id,upload_id,upload_name,location_id,union_location_id,location_name)" \
+						" VALUES " \
+						" (%s,%s,%s,%s,%s,%s)"
 
 
+			command = self.cursor.mogrify( command, (
+				self._id,
+				self.upload_id,
+				self.upload_name,
+				location_id,
+				location_id,
+				location['location_name']
+			) )
+
+			self._print_command( "CREATE collect location", command )
+			self._audit_insert( "collect location" )
+
+			self.cursor.execute( command )
+
+
+	def create_location_new( self,
+		editors_notes=None,
+		element_1_eg_room='',
+		element_2_eg_building='',
+		element_3_eg_parish='',
+		element_4_eg_city='',
+		element_5_eg_county='',
+		element_6_eg_country='',
+		element_7_eg_empire='',
+		latitude=None,
+		location_synonyms=None,
+		longitude=None,
+		notes_on_place=None,
+		):
+
+		union_location_id = None
+		location_name = [
+			element_1_eg_room,
+			element_2_eg_building,
+			element_3_eg_parish,
+			element_4_eg_city,
+			element_5_eg_county,
+			element_6_eg_country,
+			element_7_eg_empire
+		]
+
+		location_name = ", ".join([element for element in location_name if element.strip() ])
+
+		command = "INSERT INTO cofk_collect_location" \
+				" (" \
+					"_id" \
+					",editors_notes" \
+					",element_1_eg_room" \
+					",element_2_eg_building" \
+					",element_3_eg_parish" \
+					",element_4_eg_city" \
+					",element_5_eg_county" \
+					",element_6_eg_country" \
+					",element_7_eg_empire" \
+					",latitude" \
+					",location_id" \
+					",location_name" \
+					",location_synonyms" \
+					",longitude" \
+					",notes_on_place" \
+					",union_location_id" \
+					",upload_id" \
+					",upload_name" \
+				")" \
+				" VALUES " \
+				" (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+
+		location_upload_id = self.next_id()
+
+		command = self.cursor.mogrify( command, (
+			self._id,
+			editors_notes,
+			element_1_eg_room,
+			element_2_eg_building,
+			element_3_eg_parish,
+			element_4_eg_city,
+			element_5_eg_county,
+			element_6_eg_country,
+			element_7_eg_empire,
+			latitude,
+			location_upload_id,
+			location_name,
+			location_synonyms,
+			longitude,
+			notes_on_place,
+			union_location_id,
+			self.upload_id,
+			self.upload_name
+		) )
+
+		self._print_command( "CREATE collect location", command )
+		self._audit_insert( "collect location" )
+
+		self.cursor.execute( command )
+
+		return location_upload_id
+
+	def link_addressee( self,
+						iperson_id,
+						iwork_id,
+						notes_on_addressee=None ):
+
+		addressee_id = self.next_id()
+
+		command = "INSERT INTO cofk_collect_addressee_of_work" \
+				" (" \
+					"_id" \
+					",addressee_id"\
+					",iperson_id"\
+					",iwork_id"\
+					",notes_on_addressee" \
+					",upload_id" \
+				")" \
+				" VALUES " \
+				" (%s,%s,%s,%s,%s,%s)"
+
+		command = self.cursor.mogrify( command, (
+			self._id,
+			addressee_id,
+			iperson_id,
+			iwork_id,
+			notes_on_addressee,
+			self.upload_id
+		) )
+
+		self._print_command( "CREATE collect addressee link", command )
+		self._audit_insert( "collect addressee link" )
+
+		self.cursor.execute( command )
+
+	def link_author( self,
+			iperson_id,
+			iwork_id,
+			notes_on_author=None ):
+
+		author_id = self.next_id()
+
+		command = "INSERT INTO cofk_collect_author_of_work" \
+				  " (" \
+				  "_id" \
+				  ",author_id" \
+				  ",iperson_id" \
+				  ",iwork_id" \
+				  ",notes_on_author" \
+				  ",upload_id" \
+				  ")" \
+				  " VALUES " \
+				  " (%s,%s,%s,%s,%s,%s)"
+
+		command = self.cursor.mogrify( command, (
+			self._id,
+			author_id,
+			iperson_id,
+			iwork_id,
+			notes_on_author,
+			self.upload_id
+		) )
+
+		self._print_command( "CREATE collect author link", command )
+		self._audit_insert( "collect author link" )
+
+		self.cursor.execute( command )
+
+	def link_destination( self,
+			location_id,
+			iwork_id,
+			notes_on_destination=None ):
+
+		destination_id = self.next_id()
+
+		command = "INSERT INTO cofk_collect_destination_of_work" \
+				  " (" \
+				  "_id" \
+				  ",destination_id" \
+				  ",location_id" \
+				  ",iwork_id" \
+				  ",notes_on_destination" \
+				  ",upload_id" \
+				  ")" \
+				  " VALUES " \
+				  " (%s,%s,%s,%s,%s,%s)"
+
+		command = self.cursor.mogrify( command, (
+			self._id,
+			destination_id,
+			location_id,
+			iwork_id,
+			notes_on_destination,
+			self.upload_id
+		) )
+
+		self._print_command( "CREATE collect destination link", command )
+		self._audit_insert( "collect destination link" )
+
+		self.cursor.execute( command )
+
+
+	def link_origin( self,
+			location_id,
+			iwork_id,
+			notes_on_origin=None ):
+
+		origin_id = self.next_id()
+
+		command = "INSERT INTO cofk_collect_origin_of_work" \
+				  " (" \
+				  "_id" \
+				  ",origin_id" \
+				  ",location_id" \
+				  ",iwork_id" \
+				  ",notes_on_origin" \
+				  ",upload_id" \
+				  ")" \
+				  " VALUES " \
+				  " (%s,%s,%s,%s,%s,%s)"
+
+		command = self.cursor.mogrify( command, (
+			self._id,
+			origin_id,
+			location_id,
+			iwork_id,
+			notes_on_origin,
+			self.upload_id
+		) )
+
+		self._print_command( "CREATE collect origin link", command )
+		self._audit_insert( "collect origin link" )
+
+		self.cursor.execute( command )
--- docker-exporter/tweaker/upload_test.py
+++ docker-exporter/tweaker/upload_test.py
@@ -1,4 +1,5 @@
 from __future__ import print_function
+from tweaker.tweaker import DatabaseTweaker
 from tweaker.uploader import DatabaseTweakerCollect
 from config import config
 
@@ -13,28 +14,41 @@ uploader = DatabaseTweakerCollect.tweaker_from_connection( config["dbname"], con
 uploader.set_debug(True)
 
 upload_name = "Mats Upload"
-# Create upload
-upload_id = uploader.create_upload(upload_name, 1 )
+person_existing_id = 22859
+location_existing_id = 2749
+
+# Start and create upload
+uploader.start_upload( upload_name, 1 )
 
 # Create person existing
-uploader.create_person_existing( upload_id, upload_name, 22859 )
-
+uploader.create_person_existing( person_existing_id )
 # Create person new
-person_count = 1
-uploader.create_person_new( upload_id,
-							upload_name,
-							person_count,
-							"Wilcox, Mat",
+person_id = uploader.create_person_new( "Wilcox, Mat",
 							gender='M',
 							alternative_names='Matthew'
 							)
 
-work_count = 1
-uploader.create_work( upload_id,
-							upload_name,
-							work_count,
-							date_of_work_std_year='1656'
-							)
+# create existing location
+uploader.create_location_existing( location_existing_id )
+# create new location
+location_id = uploader.create_location_new(
+	element_6_eg_country='United Kingdom',
+	element_4_eg_city="Oxford" )
+
+# Create a work
+work_id = uploader.create_work( date_of_work_std_year='1656',
+	destination_id=location_existing_id,
+	origin_id=location_id )
+
+# set author
+uploader.link_author( person_id, work_id )
+# set addressee
+uploader.link_addressee( person_existing_id, work_id )
+
+# set origin
+uploader.link_origin( location_id, work_id )
+# set destination
+uploader.link_destination( location_existing_id, work_id )
 
 print()
 uploader.print_audit()
